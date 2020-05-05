7edc0dce5d0fe27f4ae35bc9d9ccd733239a4f15
Ryan McKinley <ryantxu@gmail.com>
2020-05-05 12:38:48 -0700

    added samples

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,10 +1,21 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 
 import { TimestreamQuery, TimestreamOptions } from './types';
 
 export class DataSource extends DataSourceWithBackend<TimestreamQuery, TimestreamOptions> {
+  templateSrv: any;
+
   constructor(instanceSettings: DataSourceInstanceSettings<TimestreamOptions>) {
     super(instanceSettings);
+
+    this.templateSrv = getTemplateSrv();
+  }
+
+  applyTemplateVariables(query: TimestreamQuery): TimestreamQuery {
+    return {
+      ...query,
+      rawQuery: this.templateSrv.replace(query.rawQuery),
+    };
   }
 }
--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent, ChangeEvent } from 'react';
 import { QueryEditorProps } from '@grafana/data';
-import { InlineFormLabel, LegacyForms } from '@grafana/ui';
+import { TextArea } from '@grafana/ui';
 import { DataSource } from '../DataSource';
 import { TimestreamQuery, TimestreamOptions } from '../types';
 
@@ -27,43 +27,14 @@ export class QueryEditor extends PureComponent<Props> {
     const { query } = this.props;
     return (
       <>
-        <div className="gf-form-inline">
-          <InlineFormLabel
-            width={10}
-            className="query-keyword"
-            tooltip={
-              <p>
-                The <code>spreadsheetId</code> is used to identify which spreadsheet is to be accessed or altered. This
-                ID is the value between the "/d/" and the "/edit" in the URL of your spreadsheet.
-              </p>
-            }
-          >
-            Raw Query
-          </InlineFormLabel>
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
-        </div>
         <div>
-          <textarea
+          <TextArea
             className="gf-form-input"
-            rows={10}
+            rows={15}
             value={query.rawQuery}
             onChange={this.onRawQueryChange}
-            placeholder="RAW Query"
+            placeholder="timestream query"
           />
-        </div>
-        <div className="gf-form-inline">
-          <LegacyForms.Switch
-            label="Allow Truncation"
-            tooltip="Return data (with a warning) when more than 1mb data is return"
-            labelClass={'width-10  query-keyword'}
-            checked={!(query.noTruncation === true)}
-            onChange={this.toggleNoTruncation}
-          />
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
         </div>
       </>
     );
