87c7af642d7507a235bd6c7e91575eef92912e0d
Ryan McKinley <ryantxu@gmail.com>
2020-08-06 14:53:30 -0700

    more timing info

---
+++ pkg/models/meta.go
@@ -0,0 +1,11 @@
+package models
+
+// TimestreamCustomMeta is the standard metadata
+type TimestreamCustomMeta struct {
+	StartTime  int64 `json:"executionStartTime,omitempty"`
+	FinishTime int64 `json:"executionFinishTime,omitempty"`
+
+	NextToken string `json:"nextToken,omitempty"`
+	QueryID   string `json:"queryId,omitempty"`
+	HasSeries bool   `json:"hasSeries,omitempty"`
+}
