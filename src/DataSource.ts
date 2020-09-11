b483b32d160c2f3e216f9c57e5b4cac5c51267c5
Ryan McKinley <ryantxu@gmail.com>
2020-09-11 15:02:33 -0700

    non-recursive

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -9,15 +9,10 @@ import {
   QueryResultMetaStat,
 } from '@grafana/data';
 import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
-import { Observable, of } from 'rxjs';
-import { map, concatMap, mergeMap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TimestreamQuery, TimestreamOptions, TimestreamCustomMeta, MeasureInfo, DataType } from './types';
-
-interface TimestreamRequestTracker extends TimestreamCustomMeta {
-  isDone?: boolean;
-  killed?: boolean;
-}
 
 export class DataSource extends DataSourceWithBackend<TimestreamQuery, TimestreamOptions> {
   // Easy access for QueryEditor
@@ -62,139 +57,173 @@ export class DataSource extends DataSourceWithBackend<TimestreamQuery, Timestrea
   }
 
   query(request: DataQueryRequest<TimestreamQuery>): Observable<DataQueryResponse> {
-    return this.getNextRequest(request, {
-      fetchStartTime: Date.now(),
-    } as TimestreamRequestTracker);
-  }
-
-  private getNextRequest = (
-    request: DataQueryRequest<TimestreamQuery>,
-    tracker: TimestreamRequestTracker
-  ): Observable<DataQueryResponse> => {
-    const fetchStartTime = Date.now();
     return new Observable<DataQueryResponse>(subscriber => {
-      const result = super
-        .query(request)
-        .pipe(
-          map(response => ({ response, meta: getNextTokenMeta(response) })),
-          concatMap(({ meta, response }) => {
-            if (meta === undefined) {
-              tracker.isDone = true;
-              // Mutate the first frame with custom results
-              if (response.data?.length) {
-                const first = response.data[0] as DataFrame;
-                const custom = first.meta?.custom as TimestreamCustomMeta;
-                custom.fetchStartTime = fetchStartTime;
-                custom.fetchEndTime = Date.now();
-                custom.fetchTime = custom.fetchEndTime - custom.fetchStartTime;
-                if (custom) {
-                  if (tracker.subs) {
-                    tracker.subs.push(custom);
-                    tracker.executionFinishTime = custom.executionFinishTime;
-                    tracker.hasSeries = custom.hasSeries;
-                  } else {
-                    tracker = {
-                      ...tracker,
-                      ...custom,
-                    };
-                  }
-                  delete tracker.nextToken;
-                  delete tracker.isDone;
-                  // Add stats
-                  if (tracker.executionStartTime && tracker.executionFinishTime) {
-                    const tsTime = tracker.executionFinishTime - tracker.executionStartTime;
-                    if (tsTime > 0) {
-                      const stats: QueryResultMetaStat[] = [];
-                      if (tracker.subs && tracker.subs.length) {
-                        stats.push({
-                          displayName: 'HTTP request count',
-                          value: tracker.subs.length,
-                          unit: 'none',
-                        });
-                      }
-                      stats.push({
-                        displayName: 'Execution time (Grafana server ⇆ Timestream)',
-                        value: tsTime,
-                        unit: 'ms',
-                        decimals: 2,
-                      });
-                      if (tracker.fetchStartTime) {
-                        tracker.fetchEndTime = Date.now();
-                        const dsTime = tracker.fetchEndTime - tracker.fetchStartTime;
-                        tracker.fetchTime = dsTime - tsTime;
-                        if (dsTime > tsTime) {
-                          stats.push({
-                            displayName: 'Fetch time (Browser ⇆ Grafana server w/o Timestream)',
-                            value: tracker.fetchTime,
-                            unit: 'ms',
-                            decimals: 2,
-                          });
-                          stats.push({
-                            displayName: 'Fetch overhead',
-                            value: (tracker.fetchTime / dsTime) * 100,
-                            unit: 'percent', // 0 - 100
-                          });
-                        }
-                      }
-                      first.meta!.stats = stats;
-                      first.meta!.custom = tracker;
-                    }
-                  }
-                }
-              }
-              return [of({ ...response, state: LoadingState.Done })];
-            } else {
-              const frame = response.data[0] as DataFrame;
-              const refId = frame.refId;
-              const rawQuery = frame.meta?.executedQueryString;
-              const meta = frame.meta?.custom as TimestreamCustomMeta;
-              const nextToken = meta.nextToken!; // this stream exists because we know it is valid
-              meta.fetchStartTime = fetchStartTime;
-              meta.fetchEndTime = Date.now();
-              meta.fetchTime = meta.fetchEndTime - meta.fetchStartTime;
-              const newRequest = {
-                ...request,
-                targets: [{ refId, rawQuery, nextToken } as TimestreamQuery],
-              };
-              // Another request
-              if (!tracker.subs) {
-                tracker.queryId = meta.queryId;
-                tracker.executionStartTime = meta.executionStartTime;
-                tracker.subs = [];
-              }
-              tracker.subs.push(meta);
-              frame.meta!.custom = tracker;
-              // Cleanup the history a bit
-              (meta as any).requestNumber = tracker.subs.length;
-              delete meta.nextToken; // not useful in the response
-              delete meta.queryId;
-              return [of({ ...response, state: LoadingState.Loading }), this.getNextRequest(newRequest, tracker)];
-            }
-          }),
-          mergeMap(obs => obs)
-        )
-        .subscribe(subscriber);
-      return () => {
-        if (!tracker.isDone && !tracker.killed) {
-          if (tracker.queryId) {
-            // tracker.killed = true;
-            this.postResource(`cancel`, {
-              queryId: tracker.queryId,
-            })
-              .then(v => {
-                console.log('Timestream query Canceled:', v);
-              })
-              .catch(err => {
-                err.isHandled = true; // avoid the popup
-                console.log('error killing', err);
-              });
+      let nextRequest: DataQueryRequest<TimestreamQuery> | undefined = undefined;
+      let subscription: Subscription | undefined = undefined;
+
+      let count = 1;
+      const tracker = {
+        fetchStartTime: Date.now(),
+      } as TimestreamCustomMeta;
+
+      // Single observer gets reused
+      const observer = {
+        next: (rsp: DataQueryResponse) => {
+          nextRequest = undefined;
+          const meta = getNextTokenMeta(rsp);
+          if (meta) {
+            const nextQuery = this.updateLoadingFrameStatsAndGetNextQuery(rsp, tracker);
+            nextRequest = {
+              ...request,
+              requestId: `${request.requestId}.${++count}`,
+              startTime: Date.now(),
+              targets: [nextQuery],
+            };
+
+            // The whole response is included in the results
+            subscriber.next({ ...rsp, state: LoadingState.Loading });
           } else {
-            console.log('Unable to kill query without a queryId', tracker);
+            tracker.fetchEndTime = Date.now();
+            this.updateFinalFrameStats(rsp, tracker);
+            let state = rsp.state;
+            if (state !== LoadingState.Error) {
+              state = LoadingState.Done;
+            }
+            subscriber.next({ ...rsp, state });
           }
+        },
+        error: (err: any) => {
+          subscriber.error(err);
+        },
+        complete: () => {
+          if (subscription) {
+            subscription.unsubscribe();
+            subscription = undefined;
+          }
+
+          // Let the previous request finish first
+          if (nextRequest) {
+            subscription = super.query(nextRequest).subscribe(observer);
+            nextRequest = undefined;
+          }
+        },
+      };
+
+      // First request
+      subscription = super.query(request).subscribe(observer);
+
+      return () => {
+        observer.complete();
+        console.log('UNSUBSCRIBE', request.requestId);
+
+        if (!tracker.fetchEndTime && tracker.queryId) {
+          const elapsed = Date.now() - tracker.fetchStartTime!;
+          console.log('Cancelling running timestream query', elapsed);
+
+          // tracker.killed = true;
+          this.postResource(`cancel`, {
+            queryId: tracker.queryId,
+          })
+            .then(v => {
+              console.log('Timestream query Canceled:', v);
+            })
+            .catch(err => {
+              err.isHandled = true; // avoid the popup
+              console.log('error killing', err);
+            });
         }
-        result.unsubscribe();
       };
     });
+  }
+
+  private updateLoadingFrameStatsAndGetNextQuery = (response: DataQueryResponse, tracker: TimestreamCustomMeta) => {
+    const frame = response.data[0] as DataFrame;
+    const refId = frame.refId;
+    const rawQuery = frame.meta?.executedQueryString;
+    const meta = frame.meta?.custom as TimestreamCustomMeta;
+    const nextToken = meta.nextToken!; // this stream exists because we know it is valid
+    meta.fetchStartTime = tracker.fetchStartTime;
+    meta.fetchEndTime = Date.now();
+    meta.fetchTime = meta.fetchEndTime - tracker.fetchStartTime!;
+    // Another request
+    if (!tracker.subs) {
+      tracker.queryId = meta.queryId;
+      tracker.executionStartTime = meta.executionStartTime;
+      tracker.subs = [];
+    }
+    tracker.subs.push(meta);
+    frame.meta!.custom = tracker;
+
+    // Cleanup the history a bit
+    (meta as any).requestNumber = tracker.subs.length;
+    delete meta.nextToken; // not useful in the response
+    delete meta.queryId;
+
+    return { refId, rawQuery, nextToken } as TimestreamQuery;
+  };
+
+  private updateFinalFrameStats = (response: DataQueryResponse, tracker: TimestreamCustomMeta) => {
+    if (response.data?.length) {
+      const first = response.data[0] as DataFrame;
+      const custom = first.meta?.custom as TimestreamCustomMeta;
+      custom.fetchStartTime = tracker.fetchStartTime;
+      custom.fetchEndTime = Date.now();
+      custom.fetchTime = custom.fetchEndTime - custom.fetchStartTime!;
+      if (custom) {
+        if (tracker.subs) {
+          tracker.subs.push(custom);
+          tracker.executionFinishTime = custom.executionFinishTime;
+          tracker.hasSeries = custom.hasSeries;
+        } else {
+          tracker = {
+            ...tracker,
+            ...custom,
+          };
+        }
+        delete tracker.nextToken;
+
+        // Add stats
+        if (tracker.executionStartTime && tracker.executionFinishTime) {
+          const tsTime = tracker.executionFinishTime - tracker.executionStartTime;
+          if (tsTime > 0) {
+            const stats: QueryResultMetaStat[] = [];
+            if (tracker.subs && tracker.subs.length) {
+              stats.push({
+                displayName: 'HTTP request count',
+                value: tracker.subs.length,
+                unit: 'none',
+              });
+            }
+            stats.push({
+              displayName: 'Execution time (Grafana server ⇆ Timestream)',
+              value: tsTime,
+              unit: 'ms',
+              decimals: 2,
+            });
+            if (tracker.fetchStartTime) {
+              tracker.fetchEndTime = Date.now();
+              const dsTime = tracker.fetchEndTime - tracker.fetchStartTime;
+              tracker.fetchTime = dsTime - tsTime;
+              if (dsTime > tsTime) {
+                stats.push({
+                  displayName: 'Fetch time (Browser ⇆ Grafana server w/o Timestream)',
+                  value: tracker.fetchTime,
+                  unit: 'ms',
+                  decimals: 2,
+                });
+                stats.push({
+                  displayName: 'Fetch overhead',
+                  value: (tracker.fetchTime / dsTime) * 100,
+                  unit: 'percent', // 0 - 100
+                });
+              }
+            }
+            first.meta!.stats = stats;
+            first.meta!.custom = tracker;
+          }
+        }
+      }
+    }
   };
 
   //----------------------------------------------
