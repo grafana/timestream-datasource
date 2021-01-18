06d5d66f0609f47d6032570db6d2b47f73278d94
Lucas Ewalt <lucas.ewalt@ecoation.com>
2021-01-18 11:29:34 -0800

    Correct US west coast region is Orgeon, not California

--- src/common/regions.ts
+++ src/common/regions.ts
@@ -1,6 +1,6 @@
 import { SelectableValue } from '@grafana/data';
 
-export const standardRegions: Array<SelectableValue<string>> = ['us-east-1', 'us-east-2', 'us-west-1', 'eu-west-2'].map(
+export const standardRegions: Array<SelectableValue<string>> = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-2'].map(
   r => {
     return { value: r, label: r };
   }
