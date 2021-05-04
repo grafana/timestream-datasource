d82ddb9518d688f4053549654b95f1b92048ace1
Hubert Asamer <asamerh4@users.noreply.github.com>
2021-05-04 15:27:59 +0200

    Add `eu-central-1` as supported region

--- src/regions.ts
+++ src/regions.ts
@@ -1,1 +1,1 @@
-export const standardRegions = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1'];
+export const standardRegions = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1', 'eu-central-1'];
