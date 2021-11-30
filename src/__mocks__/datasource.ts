5228959255ccbe5de81eded3d2d4500487a615f5
Andres Martinez Gotor <andres.martinez@grafana.com>
2021-11-30 12:10:47 +0100

    Quote and join multiple variables (#118)

---
+++ src/__mocks__/datasource.ts
@@ -0,0 +1,60 @@
+import { DataSourcePluginOptionsEditorProps, PluginType } from '@grafana/data';
+import { TimestreamOptions, TimestreamQuery } from '../types';
+import { DataSource } from '../DataSource';
+
+export const mockDatasource = new DataSource({
+  id: 1,
+  uid: 'timestream-id',
+  type: 'timestream-datasource',
+  name: 'Timestream Data Source',
+  jsonData: {},
+  meta: {
+    id: 'timestream-datasource',
+    name: 'Timestream Data Source',
+    type: PluginType.datasource,
+    module: '',
+    baseUrl: '',
+    info: {
+      description: '',
+      screenshots: [],
+      updated: '',
+      version: '',
+      logos: {
+        small: '',
+        large: '',
+      },
+      author: {
+        name: '',
+      },
+      links: [],
+    },
+  },
+});
+
+export const mockDatasourceOptions: DataSourcePluginOptionsEditorProps<TimestreamOptions> = {
+  options: {
+    id: 1,
+    orgId: 1,
+    name: 'Timestream',
+    typeLogoUrl: '',
+    type: '',
+    access: '',
+    url: '',
+    password: '',
+    user: '',
+    basicAuth: false,
+    basicAuthPassword: '',
+    basicAuthUser: '',
+    database: '',
+    isDefault: false,
+    jsonData: {
+      defaultRegion: 'us-east-2',
+    },
+    secureJsonFields: {},
+    readOnly: false,
+    withCredentials: false,
+  },
+  onOptionsChange: jest.fn(),
+};
+
+export const mockQuery: TimestreamQuery = { rawQuery: 'select * from foo', refId: '' };
