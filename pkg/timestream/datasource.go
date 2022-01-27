ef1d3c06c5bfcf7fec7b6b0516d920dfb3c98d86
Andres Martinez Gotor <andres.martinez@grafana.com>
2022-01-27 17:03:45 +0100

    Upgrade sdks (#132)

--- pkg/timestream/datasource.go
+++ pkg/timestream/datasource.go
@@ -7,12 +7,12 @@ import (
 
 	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	sdkhttpclient "github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/resource"
 	"github.com/grafana/timestream-datasource/pkg/models"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/timestreamquery"
 )
 
@@ -60,7 +60,23 @@ func NewServerInstance(s backend.DataSourceInstanceSettings) (instancemgmt.Insta
 		Runner: &timestreamRunner{
 			querySvc: func(region string) (client *timestreamquery.TimestreamQuery, err error) {
 
-				sess, err := sessions.GetSession(region, settings.AWSDatasourceSettings)
+				httpClientProvider := sdkhttpclient.NewProvider()
+				httpClientOptions, err := settings.Config.HTTPClientOptions()
+				if err != nil {
+					backend.Logger.Error("failed to create HTTP client options", "error", err.Error())
+					return nil, err
+				}
+				httpClient, err := httpClientProvider.New(httpClientOptions)
+				if err != nil {
+					backend.Logger.Error("failed to create HTTP client", "error", err.Error())
+					return nil, err
+				}
+
+				sess, err := sessions.GetSession(awsds.SessionConfig{
+					Settings:      settings.AWSDatasourceSettings,
+					HTTPClient:    httpClient,
+					UserAgentName: aws.String("Timestream"),
+				})
 				if err != nil {
 					return nil, err
 				}
@@ -70,10 +86,6 @@ func NewServerInstance(s backend.DataSourceInstanceSettings) (instancemgmt.Insta
 				}
 				querySvc := timestreamquery.New(sess, tcfg)
 
-				// Add the user agent version
-				querySvc.Handlers.Send.PushFront(func(r *request.Request) {
-					r.HTTPRequest.Header.Set("User-Agent", fmt.Sprintf("GrafanaTimestream/%s", "1.0.0"))
-				})
 				return querySvc, nil
 			},
 		},
