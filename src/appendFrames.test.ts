428d6786b14135fbdbe91d0eac8b5e4f4534b8bc
Andres Martinez Gotor <andres.mgotor@gmail.com>
2022-03-21 13:51:28 +0100

    review

--- src/appendFrames.test.ts
+++ src/appendFrames.test.ts
@@ -13,12 +13,12 @@ describe('appendMatchingFrames', () => {
     expect(appendMatchingFrames([], [])).toEqual([]);
   });
 
-  it('should return input MutableDataFrames', () => {
+  it('should return the unchanged input when it is a MutableDataFrames array', () => {
     const frame = new MutableDataFrame({ fields: [{ name: 'foo', values: v }] });
     expect(appendMatchingFrames([frame], [])).toEqual([frame]);
   });
 
-  it('should return input DataFrame', () => {
+  it('should return a MutableDataFrame version of input when input is a DataFrames array', () => {
     const frame: DataFrame = {
       length: 1,
       fields: [
@@ -34,8 +34,32 @@ describe('appendMatchingFrames', () => {
     expect(appendMatchingFrames([frame], [])).toEqual([expected]);
   });
 
-  it('should append new frames', () => {
+  it('should append new frames to an empty array', () => {
     const frame = new MutableDataFrame({ fields: [{ name: 'foo', values: v }] });
     expect(appendMatchingFrames([], [frame])).toEqual([frame]);
+  });
+
+  it('should append new frames to an existing array', () => {
+    const frame1 = new MutableDataFrame({ fields: [{ name: 'foo', values: v }] });
+    const frame2 = new MutableDataFrame({ fields: [{ name: 'bar', values: v }] });
+    expect(appendMatchingFrames([frame1], [frame2])).toEqual([frame1, frame2]);
+  });
+
+  it('should merge two frames values', () => {
+    const frame1 = new MutableDataFrame({ fields: [{ name: 'foo', values: v }] });
+    const v2: Vector<number> = {
+      length: 1,
+      get: () => 2,
+      toArray: () => [2],
+    };
+    const frame2 = new MutableDataFrame({ fields: [{ name: 'foo', values: v2 }] });
+    const mergedFrames = appendMatchingFrames([frame1], [frame2]);
+    expect(mergedFrames).toHaveLength(1);
+    const mergedValue: Vector<number> = {
+      length: 2,
+      get: () => 1,
+      toArray: () => [1, 2],
+    };
+    expect(mergedFrames[0]).toEqual(new MutableDataFrame({ fields: [{ name: 'foo', values: mergedValue }] }));
   });
 });
