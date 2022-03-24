62e8963c290f9b5b0e7751d5f39cdb1eaedfdea4
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-03-24 17:01:13 +0100

    Add tests

--- pkg/timestream/datasource_test.go
+++ pkg/timestream/datasource_test.go
@@ -2,6 +2,8 @@ package timestream
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -21,9 +23,17 @@ func (f *fakeSender) Send(res *backend.CallResourceResponse) error {
 type fakeRunner struct {
 	queryRunner
 	output *timestreamquery.QueryOutput
+
+	calls runnerCalls
+}
+
+type runnerCalls struct {
+	runQuery []*timestreamquery.QueryInput
 }
 
 func (f *fakeRunner) runQuery(ctx context.Context, input *timestreamquery.QueryInput) (*timestreamquery.QueryOutput, error) {
+	f.calls.runQuery = append(f.calls.runQuery, input)
+
 	return f.output, nil
 }
 
@@ -116,4 +126,37 @@ func TestCallResource(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_queries_called_with_db_name_quoted(t *testing.T) {
+	t.Run("db name provided without quotes runs query with quoted db name", func(t *testing.T) {
+		mockRunner := &fakeRunner{output: &timestreamquery.QueryOutput{Rows: []*timestreamquery.Row{}}}
+		ts := &timestreamDS{Runner: mockRunner}
+		sender := &fakeSender{}
+
+		assert.NoError(t, ts.CallResource(context.Background(), &backend.CallResourceRequest{
+			Method: "POST",
+			Path:   "tables",
+			Body:   []byte(`{"database":"db"}`),
+		}, sender))
+
+		require.Len(t, mockRunner.calls.runQuery, 1)
+		assert.Equal(t, &timestreamquery.QueryInput{QueryString: aws.String(`SHOW TABLES FROM "db"`)}, mockRunner.calls.runQuery[0])
+	})
+
+	t.Run("db name provided with quotes runs query with db name as-is", func(t *testing.T) {
+		mockRunner := &fakeRunner{output: &timestreamquery.QueryOutput{Rows: []*timestreamquery.Row{}}}
+		ts := &timestreamDS{Runner: mockRunner}
+		sender := &fakeSender{}
+
+		assert.NoError(t, ts.CallResource(context.Background(), &backend.CallResourceRequest{
+			Method: "POST",
+			Path:   "tables",
+			Body:   []byte(`{"database":"\"db\""}`),
+		}, sender))
+
+		require.Len(t, mockRunner.calls.runQuery, 1)
+		assert.Equal(t, &timestreamquery.QueryInput{QueryString: aws.String(`SHOW TABLES FROM "db"`)}, mockRunner.calls.runQuery[0])
+	})
+
 }
