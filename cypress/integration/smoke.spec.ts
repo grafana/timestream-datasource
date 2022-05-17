87b21638a007882631fed76e502ccc39fee8afba
yaellec <yaelle.chaudy@grafana.com>
2022-05-17 13:54:17 +0200

    added cy.contains instead of screenshot match

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -65,6 +65,7 @@ const addTablePanel = (q: string) => {
       cy.get('.panel-content').last().click();
       cy.get('.panel-loading');
       cy.get('.panel-loading', { timeout: 10000 }).should('not.exist');
+      cy.contains('No data in response').should('exist');
     },
   });
 
