c3bfa4262202874ec72e1dd1e637512c812e023a
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-07-22 09:05:29 -0700

    fix lint

--- src/regions.ts
+++ src/regions.ts
@@ -1,1 +1,9 @@
-export const standardRegions = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-southeast-2', 'ap-northeast-1'];
+export const standardRegions = [
+  'us-east-1',
+  'us-east-2',
+  'us-west-2',
+  'eu-west-1',
+  'eu-central-1',
+  'ap-southeast-2',
+  'ap-northeast-1',
+];
