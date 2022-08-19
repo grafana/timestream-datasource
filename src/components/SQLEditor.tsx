589f24ff544a51be5eb7fd1b107da10801db5059
Erik Sundell <erik.sundell87@gmail.com>
2022-08-19 16:39:29 +0200

    use custom language

---
+++ src/components/SQLEditor.tsx
@@ -0,0 +1,75 @@
+import { SQLEditor as SQLCodeEditor } from '@grafana/experimental';
+import { DataSource } from '../DataSource';
+import { getTimestreamCompletionProvider } from 'language/completionItemProvider';
+import { DATABASE_MACRO, TABLE_MACRO } from 'language/macros';
+import React, { useRef, useMemo, useCallback, useEffect } from 'react';
+import { TimestreamQuery } from 'types';
+import timestreamLanguageDefinition from 'language/definition';
+
+interface RawEditorProps {
+  query: TimestreamQuery;
+  onRunQuery: () => void;
+  onChange: (q: string) => void;
+  datasource: DataSource;
+}
+
+export default function SQLEditor({ query, datasource, onRunQuery, onChange }: RawEditorProps) {
+  const queryRef = useRef<TimestreamQuery>(query);
+  useEffect(() => {
+    queryRef.current = query;
+  }, [query]);
+
+  const getDatabases = useCallback(async () => {
+    const databases: string[] = await datasource.postResource('databases');
+    return databases.map((database) => ({ name: database, completion: database }));
+  }, [queryRef.current]);
+
+  const getTables = useCallback(
+    async (database?: string) => {
+      const tables: string[] = await datasource
+        .postResource('tables', {
+          database: database ?? queryRef.current.database ?? '',
+        })
+        .catch(() => []);
+      return tables.map((table) => ({ name: table, completion: table }));
+    },
+    [datasource]
+  );
+
+  const getColumns = useCallback(async (database?: string, tableName?: string) => {
+    const interpolatedArgs = {
+      database: database ? database.replace(TABLE_MACRO, queryRef.current.table ?? '') : queryRef.current.table,
+      table: tableName ? tableName.replace(DATABASE_MACRO, queryRef.current.database ?? '') : queryRef.current.database,
+    };
+    const [measures, dimensions] = await Promise.all([
+      datasource.postResource('measures', interpolatedArgs).catch(() => []),
+      datasource.postResource('dimensions', interpolatedArgs).catch(() => []),
+    ]);
+    return [...measures, ...dimensions].map((column) => ({ name: column, completion: column }));
+  }, []);
+
+  const getDatabasesRef = useRef(getDatabases);
+  const getTablesRef = useRef(getTables);
+  const getColumnsRef = useRef(getColumns);
+  const completionProvider = useMemo(
+    () =>
+      getTimestreamCompletionProvider({
+        getDatabases: getDatabasesRef,
+        getTables: getTablesRef,
+        getColumns: getColumnsRef,
+      }),
+    []
+  );
+
+  return (
+    <SQLCodeEditor
+      query={query.rawQuery ?? ''}
+      onBlur={() => onRunQuery()}
+      onChange={(rawQuery) => onChange(rawQuery)}
+      language={{
+        ...timestreamLanguageDefinition,
+        completionProvider,
+      }}
+    ></SQLCodeEditor>
+  );
+}
---
+++ src/language/completionItemProvider.ts
@@ -0,0 +1,51 @@
+import {
+  ColumnDefinition,
+  getStandardSQLCompletionProvider,
+  LanguageCompletionProvider,
+  LinkedToken,
+  TableDefinition,
+  TableIdentifier,
+} from '@grafana/experimental';
+import { MACROS } from './macros';
+
+interface CompletionProviderGetterArgs {
+  getDatabases: React.MutableRefObject<() => Promise<TableDefinition[]>>;
+  getTables: React.MutableRefObject<(d?: string) => Promise<TableDefinition[]>>;
+  getColumns: React.MutableRefObject<(database?: string, table?: string) => Promise<ColumnDefinition[]>>;
+}
+
+export const getTimestreamCompletionProvider: (args: CompletionProviderGetterArgs) => LanguageCompletionProvider = ({
+  getDatabases,
+  getTables,
+  getColumns,
+}) => (monaco, language) => {
+  return {
+    // get standard SQL completion provider which will resolve functions and macros
+    ...(language && getStandardSQLCompletionProvider(monaco, language)),
+    triggerCharacters: ['.', ' ', '$', ',', '(', "'"],
+    schemas: {
+      resolve: async () => getDatabases.current(),
+    },
+    tables: {
+      resolve: async (t: TableIdentifier) => {
+        return await getTables.current(t?.schema);
+      },
+      parseName: (token: LinkedToken) => {
+        let tablePath = token?.value ?? '';
+
+        const parts = tablePath.split('.');
+        if (parts.length === 1) {
+          return { schema: parts[0] };
+        } else if (parts.length === 2) {
+          return { schema: parts[0], table: parts[1] };
+        }
+
+        return null;
+      },
+    },
+    columns: {
+      resolve: async (t: TableIdentifier) => getColumns.current(t.schema, t.table),
+    },
+    supportedMacros: () => MACROS,
+  };
+};
