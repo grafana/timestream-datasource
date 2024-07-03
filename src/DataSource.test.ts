521b0577a900841f19fbf4da884689d5720930ef
idastambuk <ida.stambuk@grafana.com>
2024-07-03 14:36:12 +0200

    Rebase main

--- src/DataSource.test.ts
+++ src/DataSource.test.ts
@@ -5,15 +5,17 @@ import { mockDatasource, mockQuery } from './__mocks__/datasource';
 
 describe('DataSource', () => {
   describe('applyTemplateVariables', () => {
-    const scopedVars: Record<string, any> = {
-      $simple: 'foo',
-      $multiple: ['foo', 'bar'],
+    const scopedVars: ScopedVars = {
+      $simple: {value: 'foo'},
+      $multiple: {value: ['foo', 'bar']},
+      __interval_ms: {value: 5000000},
+      __interval: {value: 50000},
     };
     // simplified version of getTemplateSrv().replace
     const replace = jest.fn((target?: string, scopedVars?: ScopedVars, format?: string | Function) => {
       let res = target ?? '';
       if (scopedVars && typeof format === 'function') {
-        Object.keys(scopedVars).forEach((v) => (res = res.replace(v, format(scopedVars[v]))));
+        Object.keys(scopedVars).forEach((v) => (res = res.replace(v, format(scopedVars[v]?.value))));
       }
       return res;
     });
@@ -40,6 +42,13 @@ describe('DataSource', () => {
         scopedVars
       );
       expect(res.rawQuery).toEqual(`select * from foo where var in ('foo','bar')`);
+    });
+    it('should replace __interval interpolated variables with their original string', () => {
+      const res = mockDatasource.applyTemplateVariables(
+        { ...mockQuery, rawQuery: 'select $__interval_ms, $__interval' },
+        scopedVars
+      );
+      expect(res.rawQuery).toEqual(`select $__interval_ms, $__interval`);
     });
   });
 });
