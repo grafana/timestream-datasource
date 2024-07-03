cb425dc2ff7ac7bd016da848709f4511ba57af39
idastambuk <ida.stambuk@grafana.com>
2024-07-03 17:27:35 +0200

    Lint, test

--- src/DataSource.test.ts
+++ src/DataSource.test.ts
@@ -5,9 +5,6 @@ import { mockDatasource, mockQuery } from './__mocks__/datasource';
 
 describe('DataSource', () => {
   describe('applyTemplateVariables', () => {
-    afterEach(() => {
-      jest.resetAllMocks();
-    });
     const scopedVars: ScopedVars = {
       $simple: { value: 'foo' },
       $multiple: { value: ['foo', 'bar'] },
@@ -15,15 +12,15 @@ describe('DataSource', () => {
       __interval: { value: 50000 },
     };
     // simplified version of getTemplateSrv().replace
-    const replaceMock = jest
-      .fn()
-      .mockImplementation((target?: string, scopedVars?: ScopedVars, format?: string | Function) => {
+    const replaceMock = jest.fn().mockImplementation(
+      (target?: string, scopedVars?: ScopedVars, format?: string | Function) => {
         let res = target ?? '';
         if (scopedVars && typeof format === 'function') {
           Object.keys(scopedVars).forEach((v) => (res = res.replace(v, format(scopedVars[v]?.value))));
         }
         return res;
-      });
+      }
+    );
     beforeEach(() => {
       jest.spyOn(runtime, 'getTemplateSrv').mockImplementation(() => ({
         getVariables: jest.fn(),
@@ -31,33 +28,35 @@ describe('DataSource', () => {
         containsTemplate: jest.fn(),
         updateTimeRange: jest.fn(),
       }));
-      it('should replace a simple var', () => {
-        const res = mockDatasource.applyTemplateVariables(
-          { ...mockQuery, rawQuery: 'select * from $simple' },
-          scopedVars
-        );
-        expect(res.rawQuery).toEqual('select * from foo');
-      });
+    });
 
-      it('should replace a multiple var', () => {
-        const res = mockDatasource.applyTemplateVariables(
-          { ...mockQuery, rawQuery: 'select * from foo where var in ($multiple)' },
-          scopedVars
-        );
-        expect(res.rawQuery).toEqual(`select * from foo where var in ('foo','bar')`);
-      });
-      it('should replace __interval interpolated variables with their original string', () => {
-        mockDatasource.applyTemplateVariables(
-          { ...mockQuery, rawQuery: 'select $__interval_ms, $__interval' },
-          {
-            __interval_ms: { value: 5000000 },
-            __interval: { value: 50000 },
-          }
-        );
-        // check rawQuery.replace is called with correct interval value
-        expect(replaceMock.mock.calls[3][1].__interval).toEqual({ value: '$__interval' });
-        expect(replaceMock.mock.calls[3][1].__interval_ms).toEqual({ value: '$__interval_ms' });
-      });
+    it('should replace a simple var', () => {
+      const res = mockDatasource.applyTemplateVariables(
+        { ...mockQuery, rawQuery: 'select * from $simple' },
+        scopedVars
+      );
+      expect(res.rawQuery).toEqual('select * from foo');
+    });
+
+    it('should replace a multiple var', () => {
+      const res = mockDatasource.applyTemplateVariables(
+        { ...mockQuery, rawQuery: 'select * from foo where var in ($multiple)' },
+        scopedVars
+      );
+      expect(res.rawQuery).toEqual(`select * from foo where var in ('foo','bar')`);
+    });
+
+    it('should replace __interval interpolated variables with their original string', () => {
+      mockDatasource.applyTemplateVariables(
+        { ...mockQuery, rawQuery: 'select $__interval_ms, $__interval' },
+        {
+          __interval_ms: { value: 5000000 },
+          __interval: { value: 50000 },
+        }
+      );
+      // check rawQuery.replace is called with correct interval value
+      expect(replaceMock.mock.calls[3][1].__interval).toEqual({ value: '$__interval' });
+      expect(replaceMock.mock.calls[3][1].__interval_ms).toEqual({ value: '$__interval_ms' });
     });
   });
 });
