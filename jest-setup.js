9e3d1994c9c6eff173b9849d95e86a3b9594d738
Kevin Yu <kevinwcyu@users.noreply.github.com>
2025-02-11 14:23:03 -0800

    fix test

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,14 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  value: jest.fn(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+    takeRecords: jest.fn(),
+    root: null,
+    rootMargin: '',
+    thresholds: [],
+  })),
+});
