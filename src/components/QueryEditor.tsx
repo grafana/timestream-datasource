715e29c49265e7ea72f0a094e381c251d07a5d4a
Ida Štambuk <ida.stambuk@grafana.com>
2025-02-27 13:02:39 +0100

    Migrate form to new styling (#375)

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,6 +1,6 @@
 import { ResourceSelector } from '@grafana/aws-sdk';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
-import { InlineField, InlineSegmentGroup, Label, Select, Switch } from '@grafana/ui';
+import { Select, Switch, useStyles2 } from '@grafana/ui';
 import React, { useEffect, useState } from 'react';
 
 import { DataSource } from '../DataSource';
@@ -8,6 +8,8 @@ import { FormatOptions, SelectableFormatOptions, TimestreamOptions, TimestreamQu
 import { sampleQueries } from './samples';
 import { selectors } from './selectors';
 import SQLEditor from './SQLEditor';
+import { EditorField, EditorFieldGroup, EditorRow, EditorRows } from '@grafana/plugin-ui';
+import { css } from '@emotion/css';
 
 type Props = QueryEditorProps<DataSource, TimestreamQuery, TimestreamOptions>;
 
@@ -17,6 +19,8 @@ export function QueryEditor(props: Props) {
   const { query, datasource, onChange, onRunQuery } = props;
   const { database, table, measure, format } = query;
   const { defaultDatabase, defaultTable, defaultMeasure } = datasource.options;
+
+  const styles = useStyles2(getStyles);
 
   // pre-populate query with default data
   useEffect(() => {
@@ -102,108 +106,116 @@ export function QueryEditor(props: Props) {
   }, [database, table]);
 
   return (
-    <InlineSegmentGroup>
-      <div className="gf-form-group">
-        <h6>Macros</h6>
-        <ResourceSelector
-          id="database"
-          onChange={onChangeSelector('database')}
-          resources={databases}
-          value={database || null}
-          tooltip="Use the selected schema with the $__database macro"
-          label={selectors.components.ConfigEditor.defaultDatabase.input}
-          data-testid={selectors.components.ConfigEditor.defaultDatabase.wrapper}
-          labelWidth={11}
-          className="width-12"
-        />
-        <ResourceSelector
-          id="table"
-          onChange={onChangeSelector('table')}
-          resources={tables}
-          value={table || null}
-          tooltip="Use the selected table with the $__table macro"
-          label={selectors.components.ConfigEditor.defaultTable.input}
-          data-testid={selectors.components.ConfigEditor.defaultTable.wrapper}
-          labelWidth={11}
-          className="width-12"
-        />
-        <ResourceSelector
-          id="measure"
-          onChange={onChangeSelector('measure')}
-          resources={measures}
-          value={measure || null}
-          tooltip="Use the selected column with the $__measure macro"
-          label={selectors.components.ConfigEditor.defaultMeasure.input}
-          data-testid={selectors.components.ConfigEditor.defaultMeasure.wrapper}
-          labelWidth={11}
-          className="width-12"
-        />
-        <h6>Render</h6>
-        <InlineField
-          id={`${props.query.refId}-wait`}
-          label={'Wait for all queries'}
-          labelWidth={16}
-          style={{ alignItems: 'center' }}
-          htmlFor={`${props.query.refId}-wait-for-all-queries`}
-        >
-          <Switch
-            id={`${props.query.refId}-wait-for-all-queries`}
-            aria-labelledby={`${props.query.refId}-wait`}
-            onChange={onWaitForChange}
-            value={query.waitForResult}
-          />
-        </InlineField>
-        <h6>Frames</h6>
-        <InlineField
-          label="Format as"
-          htmlFor="format-as"
-          labelWidth={13}
-          tooltip={
-            <>
-              {
-                'Timeseries queries must have times in ascending order, which can be done by adding "ORDER BY <time field> ASC" to the query. '
-              }
-              <a
-                href="https://docs.aws.amazon.com/timestream/latest/developerguide/supported-sql-constructs.SELECT.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                See the AWS Docs for more details.
-              </a>
-            </>
-          }
-          interactive
-        >
-          <Select
-            inputId="format-as"
-            options={SelectableFormatOptions}
-            value={props.query.format || FormatOptions.Table}
-            onChange={onChangeFormat}
-            className="width-11"
-            menuShouldPortal={true}
-          />
-        </InlineField>
-        <h6>Sample queries</h6>
-        <Label description={'Selecting a sample will modify the current query'}>
-          <InlineField label="Query" labelWidth={11} htmlFor={`${props.query.refId}-sample-query`}>
-            <Select
-              aria-label={'Query'}
-              inputId={`${props.query.refId}-sample-query`}
-              options={sampleQueries}
-              onChange={(e: SelectableValue) => onQueryChange(e.value || '')}
+    <EditorRows>
+      <EditorRow>
+        <EditorFieldGroup>
+          <EditorField label="Database" tooltip="Use macro $__database to reference the selected database">
+            <ResourceSelector
+              id="database"
+              onChange={onChangeSelector('database')}
+              resources={databases}
+              value={database || null}
+              tooltip="Use the selected schema with the $__database macro"
+              label={selectors.components.ConfigEditor.defaultDatabase.input}
+              data-testid={selectors.components.ConfigEditor.defaultDatabase.wrapper}
+              labelWidth={11}
               className="width-12"
             />
-          </InlineField>
-        </Label>
-      </div>
-      <div style={{ minWidth: '400px', marginLeft: '10px', flex: 1 }}>
-        <SQLEditor
-          query={query}
-          onRunQuery={props.onRunQuery}
-          onChange={props.onChange}
-          datasource={props.datasource}
-        />
-      </div>
-    </InlineSegmentGroup>
+          </EditorField>
+          <EditorField label="Table" tooltip="Use macro $__table to reference the selected table">
+            <ResourceSelector
+              id="table"
+              onChange={onChangeSelector('table')}
+              resources={tables}
+              value={table || null}
+              tooltip="Use the selected table with the $__table macro"
+              label={selectors.components.ConfigEditor.defaultTable.input}
+              data-testid={selectors.components.ConfigEditor.defaultTable.wrapper}
+              labelWidth={11}
+              className="width-12"
+            />
+          </EditorField>
+          <EditorField label="Measure" tooltip="Use macro $__measure to reference the selected measure">
+            <ResourceSelector
+              id="measure"
+              onChange={onChangeSelector('measure')}
+              resources={measures}
+              value={measure || null}
+              tooltip="Use the selected column with the $__measure macro"
+              label={selectors.components.ConfigEditor.defaultMeasure.input}
+              data-testid={selectors.components.ConfigEditor.defaultMeasure.wrapper}
+              labelWidth={11}
+              className="width-12"
+            />
+          </EditorField>
+        </EditorFieldGroup>
+      </EditorRow>
+      <EditorRow>
+        <EditorFieldGroup>
+          <EditorField label="Wait for all queries">
+            <Switch
+              id={`${props.query.refId}-wait-for-all-queries`}
+              onChange={onWaitForChange}
+              value={query.waitForResult}
+            />
+          </EditorField>
+        </EditorFieldGroup>
+        <EditorFieldGroup>
+          <EditorField
+            label="Format as"
+            tooltipInteractive
+            tooltip={
+              <>
+                {
+                  'Timeseries queries must have times in ascending order, which can be done by adding "ORDER BY <time field> ASC" to the query. '
+                }
+                <a
+                  href="https://docs.aws.amazon.com/timestream/latest/developerguide/supported-sql-constructs.SELECT.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See the AWS Docs for more details.
+                </a>
+              </>
+            }
+          >
+            <Select
+              inputId="format-as"
+              options={SelectableFormatOptions}
+              value={props.query.format || FormatOptions.Table}
+              onChange={onChangeFormat}
+              className="width-11"
+              menuShouldPortal={true}
+            />
+          </EditorField>
+        </EditorFieldGroup>
+      </EditorRow>
+      <EditorRow>
+        <EditorField label="Sample queries" tooltip="Selecting a sample will modify the current query">
+          <Select
+            aria-label={'Query'}
+            inputId={`${props.query.refId}-sample-query`}
+            options={sampleQueries}
+            onChange={(e: SelectableValue) => onQueryChange(e.value || '')}
+            className="width-12"
+          />
+        </EditorField>
+      </EditorRow>
+      <EditorRow>
+        <div className={styles.sqlEditor}>
+          <SQLEditor
+            query={query}
+            onRunQuery={props.onRunQuery}
+            onChange={props.onChange}
+            datasource={props.datasource}
+          />
+        </div>
+      </EditorRow>
+    </EditorRows>
   );
 }
+const getStyles = () => ({
+  sqlEditor: css({
+    width: '100%',
+  }),
+});
