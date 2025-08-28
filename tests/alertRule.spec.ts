7e1b4988c1e04d31cfd68fbdf49784c96eb8ed6f
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-08-28 10:16:41 +0200

    chore: checking for Table instead

--- tests/alertRule.spec.ts
+++ tests/alertRule.spec.ts
@@ -21,11 +21,9 @@ test('should successfully create an alert rule', async ({
     `select region, avg(measure_value::double) from $__database.$__table where time between from_milliseconds(1615395600000) and from_milliseconds(1615395900000) and measure_value::double > 1 group by region limit 10`
   );
 
-  // Seems there is a race condition where the onBlur from the SQLEditor triggers a POST to '/api/v1/eval'
-  // This causes flaky errors like Error: route.fulfill: Route is already handled!
-  // In order to fix the flakiness this lets make sure we click outside the sql editor and wait for the first POST to '/api/v1/eval' before we call alertRuleEditPage.evaluate()
-  await alertRuleEditPage.ctx.page.mouse.click(0, 0); // forces a click outside the SqlEditor forcing the onBlur handler
-  await alertRuleEditPage.ctx.page.waitForRequest((req) => req.url().includes(selectors.apis.Alerting.eval)); // wait for the onBlur handlers call to '/api/v1/eval'
+  await expect(page.locator('header').filter({ hasText: 'Table' })).toBeHidden(); // Table shouldn't exist
+  await page.mouse.click(0, 0); // try to click outside SQLEditor
+  await expect(page.locator('header').filter({ hasText: 'Table' })).toBeVisible(); // Table should exist after onBlur on SQLEditor
 
   await expect(alertRuleEditPage.evaluate()).toBeOK();
 });
