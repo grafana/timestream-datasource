ba1fdd7b871e2c6a1c5d46f447acc2d70af3ff1d
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-08-28 08:21:27 +0200

    e2e: trying to fix flakiness

--- tests/alertRule.spec.ts
+++ tests/alertRule.spec.ts
@@ -20,7 +20,12 @@ test('should successfully create an alert rule', async ({
   await page.keyboard.insertText(
     `select region, avg(measure_value::double) from $__database.$__table where time between from_milliseconds(1615395600000) and from_milliseconds(1615395900000) and measure_value::double > 1 group by region limit 10`
   );
+
+  // Seems there is a racecondition where the onBlur from the SQLEditor triggers a POST to '/api/v1/eval'
+  // This causes flaky errors like Error: route.fulfill: Route is already handled!
+  // In order to fix the flakiness this lets make sure we click outside the sql editor and wait for the first POST to '/api/v1/eval' before we evalute
+  await page.mouse.click(0, 0); // forces a click outside the SqlEditor forcing the onBlur handler
+  await page.waitForRequest(selectors.apis.Alerting.eval); // wait for the onBlur handlers call to '/api/v1/eval'
+
   await expect(alertRuleEditPage.evaluate()).toBeOK();
-  // TODO: remove page.unrouteAll if onBlur handler is removed from SQLEditor
-  await page.unrouteAll({ behavior: 'ignoreErrors' });
 });
